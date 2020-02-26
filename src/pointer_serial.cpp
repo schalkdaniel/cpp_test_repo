c3b0308a3e936dfbb311c3a9fb07322693b40369
schalkdaniel <d-schalk@t-online.de>
2020-02-26 14:04:34 +0100

    update code

--- src/pointer_serial.cpp
+++ src/pointer_serial.cpp
@@ -4,12 +4,12 @@
  *
  *  @brief Pointer, saving data with serialization, first draft
  *
- *  @section DESCRIPTION 
- *  This section will be updated once the code works as expected.  
+ *  @section DESCRIPTION
+ *  This section will be updated once the code works as expected.
  *  No virtual functions in this example
  *
- *  
- * 
+ *
+ *
  **/
 
 
@@ -19,39 +19,59 @@
 #include <boost/archive/text_iarchive.hpp>
 
 
-class Car {
-public:
-template<typenme Archive>
-void serialize(Archive& ar, unsigned int version) {
-  ar & hp;
-  ar & x;
-}
+class Gear {
+  public:
+    template<typename Archive>
+    void serialize(Archive& ar, unsigned int version) { ar & v; }
 
-private:
-  int hp;
-  Gear *x; 
+    void setV (const double& _v) { v = _v; }
+    double getV () { return v; }
+
+    void status () { std::cout << "v = " << v << std::endl; }
+
+  private:
+    double v;
 };
 
-class Gear {
-public:
-  void serialize(Archive& ar, unsigned int version) {
-    ar & v;
-  }
-private:
-  double v;
+class Car {
+  public:
+    template<typename Archive>
+    void serialize(Archive& ar, unsigned int version) {
+      ar & hp;
+      ar & x;
+    }
+    void setHP (const int& _hp) { hp = _hp; }
+    void setGear (Gear* _Gear) { x = _Gear; }
+    void status () { std::cout << "hp = " << hp << " Gear with v = " << x->getV() << std::endl; }
+
+  private:
+    int hp;
+    Gear *x;
 };
 
 int main() {
-  Car c;
-  // Expectation is Car sets up the Gear.
+  // Define new Gear:
   Gear* g = new Gear();
-  c.x = g;
+  g->setV(2.5);
+  g->status();
+
+  // Expectation is Car sets up the Gear.
+  Car c;
+  c.setHP(80);
+  c.setGear(g);
+  c.status();
+
+  /*
   std::ofstream ofs("myfile.txt");
   boost::archive::text_oarchive oa(ofs);
   oa << c;
-  ofs.close()
+  ofs.close();
+
   std::ifstream ifs("myfile.txt");
   boost::archive::text_oarchive ia(ifs);
   Car c2;
   ia >> c2;
+  c2.status();
+  */
+  return 0;
 }
